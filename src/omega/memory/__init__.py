e10257173287214f868ea1fefb71b8a410d4019e
0388580298co-dot <0388580298co@gmail.com>
2026-09-16 20:38:54 +0700

    build: expose memory public API

---
+++ src/omega/memory/__init__.py
@@ -0,0 +1,5 @@
+"""OMEGA memory abstractions and local reference implementation."""
+
+from .store import HashEmbedding, InMemoryMemoryStore, InMemoryVectorStore, MemoryRecord, MemoryStore, VectorStore
+
+__all__ = ["HashEmbedding", "InMemoryMemoryStore", "InMemoryVectorStore", "MemoryRecord", "MemoryStore", "VectorStore"]
